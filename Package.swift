4809707aad871c551fb8b2bf6a96ba2b6bdec28b
Mohammed Suhail Patel <suhailpatel94@Mohammeds-MacBook-Air.local>
2025-09-17 12:16:35 +0530

    setup

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "ErikModelFramework",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ErikModelFramework",
+            targets: ["ErikModelFramework"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ErikModelFramework",
+            url: "https://github.com/suhailp-eccentric/SwiftErikFramework/releases/download/1.0.0/ErikModelFramework.xcframework.zip",
+            checksum: "PUT_CHECKSUM_HERE"
+        )
+    ]
+)
