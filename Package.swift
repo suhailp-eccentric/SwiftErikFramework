f6a16e73c30c554c55db4754d9775a88d21ebdb1
Mohammed Suhail Patel <suhailpatel94@Mohammeds-MacBook-Air.local>
2025-09-18 23:20:38 +0530

    1.0.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ErikModelFramework",
-            url: "https://github.com/suhailp-eccentric/SwiftErikFramework/releases/download/1.0.0/ErikModelFramework.xcframework.zip",
+            url: "https://github.com/suhailp-eccentric/SwiftErikFramework/releases/download/1.0.1/ErikModelFramework.xcframework.zip",
             checksum: "87330a8fb0de529ab9eeab0bceac6f23ec4aeac7a515aab9addeda19febf6e03"
         )
     ]
